977656e8189d2f9344d045b04541184db9aac2a0
emid.dev.macbook.pro <dev@emid.com.br>
2024-08-08 23:48:12 -0300

    Adicionado os profiles ao projeto para ambientes de desenvolvimento diferentes

--- src/main/java/br/com/diogotorresdev/moneyapi/api/cors/CorsFilter.java
+++ src/main/java/br/com/diogotorresdev/moneyapi/api/cors/CorsFilter.java
@@ -1,5 +1,6 @@
 package br.com.diogotorresdev.moneyapi.api.cors;
 
+import br.com.diogotorresdev.moneyapi.api.config.property.MoneyApiProperty;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,7 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    private String originPermitida = "http:localhost:8000"; //TODO: Configurar para diferentes ambientes
+    private MoneyApiProperty moneyApiProperty;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -21,10 +22,10 @@ public class CorsFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        httpResponse.setHeader("Access-Control-Allow-Origin", originPermitida);
+        httpResponse.setHeader("Access-Control-Allow-Origin", moneyApiProperty.getPermittedOrigin());
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true"); //Tem que ter para o cookie do token seja enviado
 
-        if("OPTIONS".equals(httpRequest.getMethod()) && originPermitida.equals(httpRequest.getHeader("Origin"))) {
+        if("OPTIONS".equals(httpRequest.getMethod()) && moneyApiProperty.getPermittedOrigin().equals(httpRequest.getHeader("Origin"))) {
             httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             httpResponse.setHeader("Access-Control-Allow-Headers","Authorization, Content-Type, Accept");
             httpResponse.setHeader("Access-Control-Max-Age", "3600");
